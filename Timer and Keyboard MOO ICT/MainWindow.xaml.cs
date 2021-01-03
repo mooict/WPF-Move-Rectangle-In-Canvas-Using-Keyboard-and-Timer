512c3c1c1313cb2dba9ce6d2c4b22bb94fdfc32d
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-03 18:59:35 +0000

    Add files via upload

---
+++ Timer and Keyboard MOO ICT/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+namespace Timer_and_Keyboard_MOO_ICT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        bool goLeft, goRight, goUp, goDown;
+        int playerSpeed = 8;
+        int speed = 12;
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myCanvas.Focus();
+
+            gameTimer.Tick += GameTimerEvent;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Start();
+
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            if (goLeft == true && Canvas.GetLeft(player) > 5)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) - playerSpeed);
+            }
+            if (goRight == true && Canvas.GetLeft(player) + (player.Width + 20) < Application.Current.MainWindow.Width)
+            {
+                Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed);
+            }
+            if (goUp == true && Canvas.GetTop(player) > 5)
+            {
+                Canvas.SetTop(player, Canvas.GetTop(player) - playerSpeed);
+            }
+            if (goDown == true && Canvas.GetTop(player) + (player.Height * 2) < Application.Current.MainWindow.Height)
+            {
+                Canvas.SetTop(player, Canvas.GetTop(player) + playerSpeed);
+            }
+
+            Canvas.SetLeft(box, Canvas.GetLeft(box) - speed);
+
+            if (Canvas.GetLeft(box) < 5 || Canvas.GetLeft(box) + (box.Width * 2) > Application.Current.MainWindow.Width)
+            {
+                speed = -speed;
+            }
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                goLeft = true;
+            }
+            if (e.Key == Key.Right)
+            {
+                goRight = true;
+            }
+            if (e.Key == Key.Up)
+            {
+                goUp = true;
+            }
+            if (e.Key == Key.Down)
+            {
+                goDown = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                goLeft = false;
+            }
+            if (e.Key == Key.Right)
+            {
+                goRight = false;
+            }
+            if (e.Key == Key.Up)
+            {
+                goUp = false;
+            }
+            if (e.Key == Key.Down)
+            {
+                goDown = false;
+            }
+        }
+    }
+}
